1e6d3da2526f36480b1b1f52e27d8a65da4f20f5
botf <victor.mendele68@gmail.com>
2017-09-04 19:58:42 +0200

    Added Noise Map Generator & Test Game Manager

---
+++ manage-game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ManageGame;
+
+public class GameManager : MonoBehaviour
+{
+    private MapSettings settings = new MapSettings(30,30, 2f, 0.5f, 2, new Vector2(0,0), 10, 0, true);
+    private float[,] noiseMap;
+
+    private void Start ()
+    {
+        Debug.Log("Start!");
+
+        noiseMap = NoiseMap.GenerateMap(settings.MapWidth, settings.MapHeight, settings.Scale, settings.Seed, settings.Octaves, settings.Persistance, settings.Lacunarity, settings.Offset);
+    }
+    
+    private void OnDrawGizmos ()
+    {
+        if (noiseMap != null)
+        {
+
+            Vector3 bottomLeft = transform.position - (Vector3.right * ((float)settings.MapWidth / 2f)) - (Vector3.forward * ((float)settings.MapHeight / 2f));
+
+            // debug la map
+
+            for (int i = 0; i < settings.MapWidth; i++)
+            {
+                for (int j = 0; j < settings.MapHeight; j++)
+                {
+                    // set the cellule color
+                    Gizmos.color = Color.Lerp(Color.white, Color.black, noiseMap[i,j]);
+
+                    Vector3 position = bottomLeft + Vector3.right * i + Vector3.forward * j;
+
+                    Gizmos.DrawCube(position, Vector3.one * 0.9f);
+                }
+            }
+        }
+    }
+
+    private struct MapSettings
+    {
+        private int mapWidth;
+        private int mapHeight;
+        private float persistance;  // 2
+        private float lacunarity;   // 0.5
+        private int octaves;        // 2
+        private Vector2 offset;     // 0 0
+        private float scale;        // 10
+        private int seed;
+        private bool useRandomSeed;
+
+        public MapSettings (int mapWidth, int mapHeight, float persistance, float lacunarity, int octaves, Vector2 offset, float scale, int seed, bool useRandomSeed)
+        {
+            this.mapHeight = mapHeight;
+            this.mapWidth = mapWidth;
+            this.persistance = persistance;
+            this.lacunarity = lacunarity;
+            this.octaves = octaves;
+            this.offset = offset;
+            this.scale = scale;
+            this.seed = seed;
+            this.useRandomSeed = useRandomSeed;
+        }
+
+        public float Persistance
+        {
+            get
+            {
+                return persistance;
+            }
+
+            set
+            {
+                persistance = value;
+            }
+        }
+
+        public float Lacunarity
+        {
+            get
+            {
+                return lacunarity;
+            }
+
+            set
+            {
+                lacunarity = value;
+            }
+        }
+
+        public int Octaves
+        {
+            get
+            {
+                return octaves;
+            }
+
+            set
+            {
+                octaves = value;
+            }
+        }
+
+        public Vector2 Offset
+        {
+            get
+            {
+                return offset;
+            }
+
+            set
+            {
+                offset = value;
+            }
+        }
+
+        public float Scale
+        {
+            get
+            {
+                return scale;
+            }
+
+            set
+            {
+                scale = value;
+            }
+        }
+
+        public int Seed
+        {
+            get
+            {
+                return seed;
+            }
+
+            set
+            {
+                seed = value;
+            }
+        }
+
+        public bool UseRandomSeed
+        {
+            get
+            {
+                return useRandomSeed;
+            }
+
+            set
+            {
+                useRandomSeed = value;
+            }
+        }
+
+        public int MapWidth
+        {
+            get
+            {
+                return mapWidth;
+            }
+
+            set
+            {
+                mapWidth = value;
+            }
+        }
+
+        public int MapHeight
+        {
+            get
+            {
+                return mapHeight;
+            }
+
+            set
+            {
+                mapHeight = value;
+            }
+        }
+    } 
+}
---
+++ manage-game/Assets/Scripts/NoiseMap.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+namespace ManageGame
+{
+    public static class NoiseMap
+    {
+        public static float[,] GenerateMap(int mapWidth, int mapHeight, float scale, int seed, int octaves, float persistance, float lacunarity, Vector2 offset)
+        {
+            float[,] noiseMap = new float[mapWidth, mapHeight];
+
+            // instancie un generateur de nombre aléatoire.
+            System.Random prng = new System.Random(seed);
+            Vector2[] octaveOffsets = new Vector2[octaves];
+
+            for (int i = 0; i < octaves; i++)
+            {
+                float offsetX = prng.Next(-100000, 100000) + offset.x;
+                float offsetY = prng.Next(-100000, 100000) + offset.y;
+                octaveOffsets[i] = new Vector2(offsetX, offsetY);
+            }
+
+            // prends en charge le cas ou scale vaut 0
+            // car on utilise scale pour une division.
+            if (scale <= 0)
+            {
+                scale = 0.0001f;
+            }
+
+            float maxNoiseHeight = float.MinValue;
+            float minNoiseHeight = float.MaxValue;
+
+            float halfWidth = mapWidth / 2f;
+            float halfHeight = mapHeight / 2f;
+
+            // pour chq valeur on genere un noise
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    float amplitude = 1f;
+                    float frequency = 1f;
+                    float noiseHeight = 0f;
+
+                    for (int i = 0; i < octaves; i++)
+                    {
+                        float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+                        float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                        float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2f - 1f;
+                        noiseHeight += perlinValue * amplitude;
+
+                        // amplique la persistance et la lacunarity
+                        amplitude *= persistance;
+                        frequency *= lacunarity;
+                    }
+
+                    if (noiseHeight > maxNoiseHeight)
+                    {
+                        maxNoiseHeight = noiseHeight;
+                    }
+                    else if (noiseHeight < minNoiseHeight)
+                    {
+                        minNoiseHeight = noiseHeight;
+                    }
+
+                    noiseMap[x, y] = noiseHeight;
+                }
+            }
+
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+                }
+            }
+
+            return noiseMap;
+        }
+    }
+}
